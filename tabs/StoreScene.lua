3020bea25d37630f0f2c13974121d4c58364f5b2
ZachariasOctavious at codea-scm <ZachariasOctavious@codea-scm.aws.mapote.com>
2015-11-23 11:06:19 -0500

    initial commit

---
+++ tabs/StoreScene.lua
@@ -0,0 +1,14 @@
+StoreScene = class()
+
+function StoreScene:init()
+    -- you can accept and set parameters here
+  
+end
+
+function StoreScene:draw()
+    -- Codea does not automatically call this method
+end
+
+function StoreScene:touched(touch)
+    -- Codea does not automatically call this method
+end
